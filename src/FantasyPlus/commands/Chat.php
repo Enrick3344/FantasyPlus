0991885c83c6097e2129c99c36f67f56c40749be
Enrick3344 <enrico3815@hotmail.fr>
2017-08-17 15:03:06 -0400

    Update Chat.php

--- src/FantasyPlus/commands/Chat.php
+++ src/FantasyPlus/commands/Chat.php
@@ -25,16 +25,16 @@ class Chat extends Command{
 		if(isset($args[0])){
 			switch(strtolower($args[0])){
         case "on":{
-          $this->plugin->config->get("Disabled-Chat");
-          $this->plugin->config->set("Disaled-Chat", false);
+          $this->plugin->config->get("Disable-Chat");
+          $this->plugin->config->set("Disable-Chat", false);
           $this->plugin->config->save();
           $sender->sendMessage("§l§5>§r§d Chat Has Been Enabled!");
           $this->plugin->getLogger()->notice(TextFormat::LIGHT_PURPLE . "Chat Has Been Enabled!");
         }
         break;
         case "off":{
-          $this->plugin->config->get("Disabled-Chat");
-          $this->plugin->config->set("Disaled-Chat", true);
+          $this->plugin->config->get("Disable-Chat");
+          $this->plugin->config->set("Disable-Chat", true);
           $this->plugin->config->save();
           $sender->sendMessage("§l§5>§r§d Chat Has Been Disabled!");
           $this->plugin->getLogger()->notice(TextFormat::LIGHT_PURPLE . "Chat Has Been Disabled!");
