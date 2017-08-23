7c4b5493c1b2ed9efbb3f9970ac5c6bc48625a45
Enrick3344 <enrico3815@hotmail.fr>
2017-08-23 16:57:45 -0400

    Last Update.

--- src/FantasyPlus/commands/Os.php
+++ src/FantasyPlus/commands/Os.php
@@ -1,14 +1,41 @@
-<?php
+<?php 
+/*
+ * 
+ *  _____           _                  ____  _           
+ * |  ___|_ _ _ __ | |_ __ _ ___ _   _|  _ \| |_   _ ___ 
+ * | |_ / _` | '_ \| __/ _` / __| | | | |_) | | | | / __|
+ * |  _| (_| | | | | || (_| \__ \ |_| |  __/| | |_| \__ \
+ * |_|  \__,_|_| |_|\__\__,_|___/\__, |_|   |_|\__,_|___/
+ * 				 |___/    
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ *
+ * @author Enrick Fortier
+ * 
+ * Github: https://github.com/Enrick3344
+ * Version: v0.1
+ *
+*/ 
 
 namespace FantasyPlus\commands;
 
-use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
 use pocketmine\utils\TextFormat;
-use pocketmine\command\{Command, CommandSender};
+//Plugins Files.
 use FantasyPlus\Main;
 
 class Os extends Command {
+    /** @var Main */
+    private $plugin;
 	
+    /**
+     * @param Main $plugin
+    */
    public function __construct(Main $plugin){
         parent::__construct("os", "Displays OS information", null, ["os"]);
         $this->setPermission("fantasyplus.command.os");
@@ -29,13 +56,13 @@ class Os extends Command {
 	  	  	$verinfo = php_uname("v");
 	  	  	$releasen = php_uname("r");
 	  	  	$architect = php_uname("m");
-	  	  	$sender->sendMessage(TextFormat::GOLD . "------- Server OS Info -------");
-	  	  	$sender->sendMessage(TextFormat::GOLD . "-> Server OS: " . TextFormat::AQUA .  $operatesys);
-	  	  	$sender->sendMessage(TextFormat::GOLD . "-> Kernel Build Date: " . TextFormat::AQUA . $verinfo);
-	  	        $sender->sendMessage(TextFormat::GOLD . "-> Kernel Version: " . TextFormat::AQUA . $releasen);
-	  	        $sender->sendMessage(TextFormat::GOLD . "-> Server Architecture: " . TextFormat::AQUA . $architect);
-	  	        $sender->sendMessage(TextFormat::GOLD . "----------------------------");
-		  	$log = fopen("Eofla-OSRequestLog.txt","a+");
+	  	  	$sender->sendMessage("§l§5>§d-------§r§bServer OS Info§l§d-------§5<");
+	  	  	$sender->sendMessage("§l§5>§r§b Server OS: §f" . $operatesys);
+	  	  	$sender->sendMessage("§l§5>§r§b Kernel Build Date: §f" . $verinfo);
+	  	        $sender->sendMessage("§l§5>§r§b Kernel Version: §f" . $releasen);
+	  	        $sender->sendMessage("§l§5>§r§b Server Architecture: §f" . $architect);
+	  	        $sender->sendMessage("§l§5>§d---------------------------§5<");
+		  	$log = fopen("FantasyPlus_OSRequestLog.txt","a+");
 	  	        fwrite($log, "[" .  date("Y/m/d l h:i:s a") . "] " . $sender->getName() . " requested our server info.\n");
 	  	        fclose($log);
 	  	   return true;
